fae131653f15cff19ac2520cf48830477a9352cf
dragan <dragovanmarkic@gmail.com>
2018-10-05 23:01:07 +0200

    avec api test
    avec api test

--- pendu3.js
+++ pendu3.js
@@ -1,3 +1,41 @@
+// Create a request variable and assign a new XMLHttpRequest object to it.
+var request = new XMLHttpRequest();
+
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://od-api.oxforddictionaries.com/api/v1', true);
+
+
+
+var Dictionary = require("oxford-dictionary-api");
+
+var app_id = "5b563cea";
+
+var app_key = "77115b6a45f6296ae089fdf88ebd3642";
+
+var dict = new Dictionary(app_id,app_key);
+
+dict.find("ace",function(error,data){ if(error) return console.log(error); console.log(data); });
+request.onload = function () {
+  // Begin accessing JSON data here
+  }
+
+
+// Send request
+request.send();
+
+// Begin accessing JSON data here
+var data = JSON.parse(this.response);
+
+if (request.status >= 200 && request.status < 400) {
+  data.forEach(movie => {
+    console.log(movie.title);
+  });
+} else {
+  console.log('error');
+}
+
+    
+
 words = ["hello", "coucou", "welcome"];
 
 let solution = words[Math.floor(Math.random() * words.length)];
